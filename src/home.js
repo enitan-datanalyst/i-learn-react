0802337b86b85acdf7b671b142945dfaa8866aed
enitanadata <empressbee0@gmail.com>
2023-08-28 07:21:41 -0700

    Learn click-event

--- src/home.js
+++ src/home.js
@@ -1,7 +1,28 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [name, setName] = useState('Abosede')
+    const [age, setAge] = useState(20)
+
+
+    const handleClick = () => {
+        console.log('hello, welcome');
+    }
+    const handleClickAgain = (name) => {
+        console.log('hello ' + name)
+    }
+    const clickMe = () => {
+        setName ('Enitan');
+        setAge(30);
+    }
+
     return (  
         <div className="homepage">
             <h1>Homepage</h1>
+            <button onClick={handleClick}>click me</button>
+            <button onClick={() => handleClickAgain('enny')}>click me again</button>
+            <button onClick={clickMe}>click</button>
+            <p>{name} is {age} years old</p>
         </div>
     );
 }
